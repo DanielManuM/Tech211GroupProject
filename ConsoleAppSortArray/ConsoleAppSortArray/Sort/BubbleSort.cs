18bd9332b6b58b4b39403de64dddf1301429ae15
Vlad <VStoyanov@spartaglobal.com>
2023-04-17 15:54:32 +0100

    bubble and merge sort

--- ConsoleAppSortArray/ConsoleAppSortArray/Sort/BubbleSort.cs
+++ ConsoleAppSortArray/ConsoleAppSortArray/Sort/BubbleSort.cs
@@ -2,9 +2,26 @@
 
 using System;
 
-public class Class1
+public class BubbleSort
 {
-	public Class1()
-	{
-	}
+    public static int[] BubbleSort(int[] inputArray)
+    {
+        int n = inputArray.Length;
+        bool swapped = false;
+        do
+        {
+            swapped = false;
+            for (int i = 1; i <= n - 1; i++)
+            {
+                if (inputArray[i - 1] > inputArray[i])
+                {
+                    int a = inputArray[i - 1];
+                    inputArray[i - 1] = inputArray[i];
+                    inputArray[i] = a;
+                    swapped = true;
+                }
+            }
+        } while (swapped);
+        return inputArray;
+    }
 }
