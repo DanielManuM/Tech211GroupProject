11d65fc6b7cb2a5ade6b1df24455b0ed5f02ff02
DanieManuM <dmanu@spartaglobal.com>
2023-04-17 18:13:43 +0100

    Updated InputCollector

--- ConsoleAppSortArray/ConsoleAppSortArray/InputCollector.cs
+++ ConsoleAppSortArray/ConsoleAppSortArray/InputCollector.cs
@@ -4,5 +4,44 @@ using System;
 
 public class InputCollector
 {
+    public static void InputTextDisplayArray()
+    {
+        Console.WriteLine("Please Choose the length of the array");
+    }
+
+    public static int InputArrayLength()
+    {
+
+        bool isNumber = Int32.TryParse(Console.ReadLine(), out int value);
+        if (isNumber == false)
+        {
+            throw new FormatException("Please input numbers!!");
+        }
+        else
+        {
+            return value;
+        }
+    }
+
+    public static void InputTextDisplaySort()
+    {
+        Console.WriteLine("Press 1 for BubbleSort");
+        Console.WriteLine("Press 2 for MergeSort");
+        Console.WriteLine("Press 3 for DotNetSort");
+    }
+
+    public static int InputSortType()
+    {
+
+        bool isNumber = Int32.TryParse(Console.ReadLine(), out int value);
+        if (isNumber == false)
+        {
+            throw new FormatException("Please input numbers!!");
+        }
+        else
+        {
+            return value;
+        }
+    }
 
 }
