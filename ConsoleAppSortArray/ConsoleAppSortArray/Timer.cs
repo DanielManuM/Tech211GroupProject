3c7bbe178c2e8d6dcc96211a3b7b2c9fd0ad451e
Vlad <VStoyanov@spartaglobal.com>
2023-04-17 17:39:36 +0100

    Timer

--- ConsoleAppSortArray/ConsoleAppSortArray/Timer.cs
+++ ConsoleAppSortArray/ConsoleAppSortArray/Timer.cs
@@ -1,10 +1,15 @@
 ﻿namespace ConsoleAppSortArray;
 
 using System;
+using System.Diagnostics;
 
 public class Timer
 {
-   
+    public static TimeSpan Time(Action action)
+    {
+        var stopwatch = Stopwatch.StartNew();
+        action();
+        stopwatch.Stop();
+        return stopwatch.Elapsed;
+    }
 }
-
-
